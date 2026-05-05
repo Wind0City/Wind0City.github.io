5e1364d56fcc0c661ed1cfde72427a8fc305bacf
windcity <windcity@windcitydeMac-mini.local>
2026-05-05 14:47:18 +0800

    refactor: 优化 App.tsx 结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router/indext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { StatusProvider } from "./contexts/StatusContext";
 
 function App() {
     return (
-        <>
-            {/* 背景图片 */}
-            <div
-                className="fixed inset-0 w-full h-full bg-cover bg-center bg-no-repeat pointer-events-none"
-                style={{
-                    backgroundImage: "url(/background.png)",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
-            />
-            <div className="relative z-10">
-                <RouterProvider router={router} />
-            </div>
-        </>
+        <ThemeProvider>
+            <StatusProvider>
+                {/* 背景图片 */}
+                <div
+                    className="fixed inset-0 w-full h-full bg-cover bg-center bg-no-repeat pointer-events-none transition-opacity duration-300"
+                    style={{
+                        backgroundImage: "url(/background.png)",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                />
+                {/* 暗色模式遮罩 */}
+                <div className="fixed inset-0 w-full h-full pointer-events-none bg-black/0 dark:bg-black/50 transition-colors duration-300" />
+                <div className="relative z-10">
+                    <RouterProvider router={router} />
+                </div>
+            </StatusProvider>
+        </ThemeProvider>
     );
 }
 
